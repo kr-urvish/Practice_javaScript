cbcf7df2457db7a3bb1a13d15b29ea66f343299f
urvish <urvish.r@knackroot.com>
2023-01-27 18:08:49 +0530

    commited on 27-jan

---
+++ export_t.js
@@ -0,0 +1,1 @@
+export const capstr = str => str.toUpperCase();
---
+++ import_file.js
@@ -0,0 +1,6 @@
+import {capstr} from "./export_t"
+
+//const urv = require('export_t');
+
+const cap = capstr("dfghjkl");
+console.log(cap);
